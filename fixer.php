977503f8a605a9c46ac7eb4d58dbc449fc52a354
ksjogo <jogo@kronberger-spiele.de>
2017-07-11 13:29:50 +0200

    fix ssl

--- fixer.php
+++ fixer.php
@@ -1,4 +1,10 @@
 <?php
-$config = include('./config.php');
+$config = include('config.php');
 $conn = new mysqli($config['db']['host'], $config['db']['username'], $config['db']['password'], $config['db']['dbname']);
-echo "fixed";
+$uri =  $conn->real_escape_string(getenv('SHOPWARE_DOMAIN'));
+$sql = 'UPDATE s_core_shops SET secure = true, secure_host = "' . $uri . '", always_secure = true';
+if ($conn->query($sql)) {
+    printf("%d Row inserted.\n", $conn->affected_rows);
+} else {
+    printf("Error: %s\n", $conn->sqlstate);
+}
